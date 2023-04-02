1538eb686af21ff07d5f1368bb58d23b20832932
Matheus Catel <catelmatt@hotmail.com>
2023-04-02 19:02:38 +0000

    Fp: Shellscrips Updated & Fixed

--- input/scripts/pre_process/create_env_all.sh
+++ input/scripts/pre_process/create_env_all.sh
@@ -10,10 +10,37 @@ DADOS=("clientes" "divisao" "endereco" "regiao" "vendas")
 
 for i in "${DADOS[@]}"
 do
+
+    cd ../../raw
+    echo "#############################"
 	echo "$i"
-    cd ../../raw/
+    echo "Criando datalake de $i"
     hdfs dfs -mkdir /datalake/raw/$i
+    echo "#############################"
+    echo "datalake de $i criado, agora dando permissões"
     hdfs dfs -chmod 777 /datalake/raw/$i
+    echo "#############################"
+    echo "Permissões de $i concedidas, copiando arquvios para o datalake"
     hdfs dfs -copyFromLocal $i.csv /datalake/raw/$i
-    beeline -u jdbc:hive2://localhost:10000 -f ../../scripts/hql/create_table_$i.hql 
+    echo "#############################"
+    echo "arquivo $i.csv copiado para datalake"
+    echo "#############################"
 done
+
+hdfs dfs -ls /datalake/raw/
+echo "Operação finalizada"
+
+echo "Iniciando operação de criação de tabelas"
+beeline -u jdbc:hive2://localhost:10000
+echo "Acessando database minsait"
+use database minsait;
+
+for i in "${DADOS[@]}"
+do  
+    echo "#############################"
+    echo "criando tabela de $i"
+    beeline -f ../../scripts/hql/create_table_$i.hql;
+    echo "#############################"
+done
+
+show tables;
